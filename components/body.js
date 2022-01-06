635612fcd523fc1bd8f3eb13ced455428a504d45
unknown <remael99@gmail.com>
2022-01-07 01:32:24 +0300

    add:image alton not working

--- components/body.js
+++ components/body.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import Tabs from "./Tabs";
 
@@ -8,7 +9,26 @@ export default function Body() {
         {/*tab*/}
         <h2 className="m-auto font-semibold ">Best selling product</h2>
         <Tabs />
-        <div className="my-2 w-full"></div>
+        <div className="my-2 mt-4  w-full md:w-4/5 px-6  py-2 md:h-[400px] m-auto  flex gap-4">
+          <div className="w-1/2 bg-white flex flex-col mt-5">
+            <h2 className="font-semibold text-gray-800">Why Alton ?</h2>
+            <p className=" text-gray-600 leading-7">
+              You can entrust all purchases of sports equipment to us we
+              gaurantee that all items sold by the store are one hundred
+              original and from well known and trusted labels and companies{" "}
+            </p>
+          </div>
+          <div className="w-1/2 h-full bg-white">
+            <Image
+              src="/image/alton.jpg"
+              alt="alton"
+              height={1000}
+              width={1000}
+              loading="lazy"
+              className="object-cover"
+            />
+          </div>
+        </div>
       </section>
     </main>
   );
